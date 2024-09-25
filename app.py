4a4498e4a8539b08b0a61010f14da2837194a44c
Elvis_Oliveira <jekir90@gmail.com>
2024-09-25 12:29:34 -0700

    Update script to greet user by name

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ def greet(name):
  return f"Hello, {name}!"
 if __name__ == "__main__":
  print(greet("World"))
+
+# app.py
+def greet(name):
+ return f"Hello, {name}!"
+if __name__ == "__main__":
+ user_name = input("Enter your name: ")
+ print(greet(user_name))
